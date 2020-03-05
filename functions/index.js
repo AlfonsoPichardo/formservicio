27ef29f1fca35104b8b018e223ab9bc1613fce39
Alfonso Pichardo <pichardovilchisalfonso@gmail.com>
2020-03-04 20:11:33 -0600

    ejemplo prof commit

--- functions/index.js
+++ functions/index.js
@@ -3,7 +3,7 @@
 const functions = require('firebase-functions');
 
 /* Se exporta la función sobre https llamada "saludo". */
-exports.recomendar = functions.https.onRequest(
+exports.saludo = functions.https.onRequest(
   /** Código para la función saludo.
    * @param {{query:Object}} request solicitud que recibe el servidor.
    * Corresponde a la librería Express.
@@ -12,19 +12,19 @@ exports.recomendar = functions.https.onRequest(
   (request, response) => {
     try {
       // verifica que el parámetro nombre1 recibido del navegador esté correcto.
-      if (!request.query.estado) {
+      if (!request.query.nombre1) {
         // Entra aquí si el nombre1 es null, undefined o ""
-        throw new Error("Falta el estado");
-      } else if (!request.query.descr) {
+        throw new Error("Falta el nombre 1");
+      } else if (!request.query.nombre2) {
         // Entra aquí si el nombre2 es null, undefined o ""
-        throw new Error("Falta la descripcion");
+        throw new Error("Falta el nombre 2");
       }
       /* Solo se llega a esta parte si nombre1 y nombre 2 tienen un texto.
        * Devuelve un saludo. */
       response.send(
-        `El estado: ${request.query.estado} es:  ${request.query.descr}`);
+        `Saludos a ${request.query.nombre1} y a ${request.query.nombre2}`);
     } catch (e) {
       // Devuelve un texto de error.
       response.send(e.message);
     }
-});
+  });
